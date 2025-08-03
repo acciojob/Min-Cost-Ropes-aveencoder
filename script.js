36afd37a2699519671269679534e84954bc3dd93
aveencoder <aveen598@gmail.com>
2025-08-03 18:02:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function mincost(arr)
+function mincost(arr1)
 { 
-//write your code here
-// return the min cost
+  arr1.sort((a,b)=> a-b );
+      sum = 0;
+     while(arr1.length>1){
+      const  first = arr1.shift();
+      const  second = arr1.shift();
+      const  cost = first+second;
+
+      sum+= cost;
+
+     arr1.push(cost);
+     arr1.sort((a,b)=>a-b);
+}
+     
+     return sum;
   
 }
 
